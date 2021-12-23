f4d2594038b184f613152f08a360e122c248cfa8
Tim Quimbo <tquimbo1@gmail.com>
2021-12-23 18:01:19 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,49 @@
-let state;
+// let state;
+
+// function reducer(state = { count: 0 }, action) {
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return { count: state.count + 1 };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// function dispatch(action){
+//   state = reducer(state, action);
+//   render();
+// };
+
+// function render() {
+//   let container = document.getElementById('container');
+//   container.textContent = state.count;
+// };
+
+// dispatch({ type: '@@INIT' })
+// let button = document.getElementById('button');
+
+// button.addEventListener('click', function() {
+//     dispatch({ type: 'INCREASE_COUNT' });
+// })
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -8,21 +53,18 @@ function reducer(state = { count: 0 }, action) {
     default:
       return state;
   }
-};
+}
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(reducer) // createStore takes the reducer as an argument
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
